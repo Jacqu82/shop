5945c07b8fdd0b641fab850533b7309b8736ab6b
sgr13 <sgr13@student.edu>
2017-03-19 13:19:17 +0100

    plik connection

---
+++ connection.php
@@ -0,0 +1,9 @@
+<?php
+
+include_once 'config.php';
+
+$connection = new mysqli($host, $user, $password, $database);
+
+if ($connection->connect_error) {
+    die ("Connection error" . $connection->connect_errno);
+}

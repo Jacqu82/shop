7945d0375b9c65bfccbbbd70fef6ff097af485b6
sgr13 <sgr13@student.edu>
2017-03-19 14:13:33 +0100

    modified connection

--- connection.php
+++ connection.php
@@ -7,3 +7,5 @@ $connection = new mysqli($host, $user, $password, $database);
 if ($connection->connect_error) {
     die ("Connection error" . $connection->connect_errno);
 }
+
+$connection->query('SET CHARACTER SET utf8');

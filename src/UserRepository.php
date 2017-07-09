24032fbfa7a8b0bd75fa88c2da9a92f38f6e6a50
sgr13 <sgr13@student.edu>
2017-07-09 03:21:45 +0200

    UserRepository in progress

---
+++ src/UserRepository.php
@@ -0,0 +1,19 @@
+<?php
+
+class UserRepository
+{
+    public static function saveUser()
+    {
+
+    }
+
+    public static function updateUser()
+    {
+
+    }
+
+    public static function getUserById()
+    {
+
+    }
+}

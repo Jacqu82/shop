c2dd3033a083765701caab6b279f201171aa5874
sgr13 <sgr13@student.edu>
2017-07-11 23:28:28 +0200

    funckjonalnosci admin  przeniesione do src/admin/.

--- addGroupOfProducts.php
+++ addGroupOfProducts.php
@@ -1,55 +1,55 @@
 <?php
-
-include_once 'connection.php';
-include_once 'config.php';
-require_once 'autoload.php';
-require_once 'layout/Layout.php';
-
-session_start();
-
-if (!isset($_SESSION['admin'])) {
-    header('Location: index.php');
-}
-?>
-    <html>
-    <?php Layout::showHeadInMain(); ?>
-    <body>
-    <div class="container">
-        <?php Layout::adminTopBar(); ?>
-        <p><a href='groupsOfProducts.php'><--Powrót</a></p>
-        <div class="wrapper">
-            <form action="addGroupOfProducts.php" method="post">
-                <p>Wprowadź nazwę grupy</p>
-                <input type="text" name="name"/><br>
-                <p>Wprowadź opis grupy</p>
-                <textarea type="text" name="description" rows="5" cols="40"></textarea>
-                <br>
-                <input type="submit" value="Dodaj"/>
-            </form>
-        </div>
-    </body>
-    </html>
+//
+//include_once 'connection.php';
+//include_once 'config.php';
+//require_once 'autoload.php';
+//require_once 'layout/Layout.php';
+//
+//session_start();
+//
+//if (!isset($_SESSION['admin'])) {
+//    header('Location: index.php');
+//}
+//?>
+<!--    <html>-->
+<!--    --><?php //Layout::showHeadInMain(); ?>
+<!--    <body>-->
+<!--    <div class="container">-->
+<!--        --><?php //Layout::adminTopBar(); ?>
+<!--        <p><a href='groupsOfProducts.php'><--Powrót</a></p>-->
+<!--        <div class="wrapper">-->
+<!--            <form action="addGroupOfProducts.php" method="post">-->
+<!--                <p>Wprowadź nazwę grupy</p>-->
+<!--                <input type="text" name="name"/><br>-->
+<!--                <p>Wprowadź opis grupy</p>-->
+<!--                <textarea type="text" name="description" rows="5" cols="40"></textarea>-->
+<!--                <br>-->
+<!--                <input type="submit" value="Dodaj"/>-->
+<!--            </form>-->
+<!--        </div>-->
+<!--    </body>-->
+<!--    </html>-->
 <?php
-
-if ($_SERVER['REQUEST_METHOD'] == "POST") {
-    if (isset($_POST['name']) && isset($_POST['description'])) {
-
-        $name = mysqli_real_escape_string($connection, $_POST['name']);
-        $description = mysqli_real_escape_string($connection, $_POST['description']);
-
-        //wywołujemy metodę addGroup klasy Admin w celu dodania grupy produktów do bazy danych
-        $group = AdminRepository::addGroup($connection, $name, $description);
-
-        if ($group) {
-            ?>
-            <div class='wrapper'>
-                Dodano nową grupę produktów.</br><b><?php echo $name ?></b><br><br>
-                <a href='adminPanel.php'>Panel administratora</a><br>
-                <a href='groupsOfProducts.php'>Grupy produktów</a><br>
-            </div>
-            <?php
-        } else {
-            die("Błąd dodawania grupy do bazy danych!" . $connection->errno);
-        }
-    }
-}
+//
+//if ($_SERVER['REQUEST_METHOD'] == "POST") {
+//    if (isset($_POST['name']) && isset($_POST['description'])) {
+//
+//        $name = mysqli_real_escape_string($connection, $_POST['name']);
+//        $description = mysqli_real_escape_string($connection, $_POST['description']);
+//
+//        //wywołujemy metodę addGroup klasy Admin w celu dodania grupy produktów do bazy danych
+//        $group = AdminRepository::addGroup($connection, $name, $description);
+//
+//        if ($group) {
+//            ?>
+<!--            <div class='wrapper'>-->
+<!--                Dodano nową grupę produktów.</br><b>--><?php //echo $name ?><!--</b><br><br>-->
+<!--                <a href='adminPanel.php'>Panel administratora</a><br>-->
+<!--                <a href='groupsOfProducts.php'>Grupy produktów</a><br>-->
+<!--            </div>-->
+<!--            --><?php
+//        } else {
+//            die("Błąd dodawania grupy do bazy danych!" . $connection->errno);
+//        }
+//    }
+//}
--- groupsOfProducts.php
+++ groupsOfProducts.php
@@ -1,53 +1,53 @@
 <?php
-
-include_once 'connection.php';
-include_once 'config.php';
-require_once 'autoload.php';
-require_once 'layout/Layout.php';
-
-session_start();
-
-//sprawdzenie czy użytkownik jest zalogowany
-if (!isset($_SESSION['admin'])) {
-    header('Location: web/index.php');
-}
-?>
-<html>
-<?php Layout::showHeadInMain(); ?>
-<body>
-<div class="container">
-    <?php
-    Layout::adminTopBar();
-    ?>
-    <p><a href='adminPanel.php'><--Powrót</a></p>
-    <p><a href='addGroupOfProducts.php'>Dodaj nową grupę produktów</a></p>
-
-    <!--    wykorzystanie metody która wybiera wszystkie grupy produktów z bazy danych-->
-    <?php
-    $result = SqlQueries::getGallery($connection);
-    ?>
-
-    <!--    wyświetlenie tabeli ze wszystkimi grupami produktów-->
-    <div class='tableShow'>
-        <table>
-            <tr>
-                <th>id</th>
-                <th>Nazwa</th>
-                <th>Opis</th>
-                <th>Edytuj</th>
-                <th>Usuń</th>
-            </tr>
-            <?php
-            foreach ($result as $value) {
-                $id = $value['id'];
-                echo "<tr>";
-                echo "<td>" . $value['id'] . "</td><td>" . $value['groupName'] . "</td><td>" . $value['groupDescriptiopn'] . "</td>";
-                echo "<td><a href='editGroupOfProducts.php?id=$id'>Edytuj</a></td>";
-                echo "<td><a href='deleteGroupOfProducts.php?id=$id'>Usuń</a></td></tr>";
-            }
-            ?>
-        </table>
-        <?php $connection->close(); ?>
-    </div>
-</body>
-</html>
+//
+//include_once 'connection.php';
+//include_once 'config.php';
+//require_once 'autoload.php';
+//require_once 'layout/Layout.php';
+//
+//session_start();
+//
+////sprawdzenie czy użytkownik jest zalogowany
+//if (!isset($_SESSION['admin'])) {
+//    header('Location: web/index.php');
+//}
+//?>
+<!--<html>-->
+<?php //Layout::showHeadInMain(); ?>
+<!--<body>-->
+<!--<div class="container">-->
+<!--    --><?php
+//    Layout::adminTopBar();
+//    ?>
+<!--    <p><a href='adminPanel.php'><--Powrót</a></p>-->
+<!--    <p><a href='addGroupOfProducts.php'>Dodaj nową grupę produktów</a></p>-->
+<!---->
+<!--    <!--    wykorzystanie metody która wybiera wszystkie grupy produktów z bazy danych-->-->
+<!--    --><?php
+//    $result = SqlQueries::getGallery($connection);
+//    ?>
+<!---->
+<!--    <!--    wyświetlenie tabeli ze wszystkimi grupami produktów-->-->
+<!--    <div class='tableShow'>-->
+<!--        <table>-->
+<!--            <tr>-->
+<!--                <th>id</th>-->
+<!--                <th>Nazwa</th>-->
+<!--                <th>Opis</th>-->
+<!--                <th>Edytuj</th>-->
+<!--                <th>Usuń</th>-->
+<!--            </tr>-->
+<!--            --><?php
+//            foreach ($result as $value) {
+//                $id = $value['id'];
+//                echo "<tr>";
+//                echo "<td>" . $value['id'] . "</td><td>" . $value['groupName'] . "</td><td>" . $value['groupDescriptiopn'] . "</td>";
+//                echo "<td><a href='editGroupOfProducts.php?id=$id'>Edytuj</a></td>";
+//                echo "<td><a href='deleteGroupOfProducts.php?id=$id'>Usuń</a></td></tr>";
+//            }
+//            ?>
+<!--        </table>-->
+<!--        --><?php //$connection->close(); ?>
+<!--    </div>-->
+<!--</body>-->
+<!--</html>-->
